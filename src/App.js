1984b96650ac29f07a830c74a7381f71a1c1905d
ChrisPendarvis <ChristopherPendarvis@outlook.com>
2022-11-10 13:15:15 -0500

    Added reducer.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import SpotifyWebApi from "spotify-web-api-js";
 import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import Player from ".Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -12,14 +17,38 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      console.log("[token]", token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+      spotify.getPlaylist("37i9dQZF1E34Ucml4HHx1w").then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
+      });
     }
-
-    console.log("token", _token);
-
   }, []);
 
-  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
+  return (
+    <div className="App">
+      {token ? <Player spotfiy={spotify} /> : <Login />}
+    </div>
+  ); 
 
 }
 
